3f653bb78a62758e7ab471973b4249cdf420e35a
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-17 16:12:03 +0200

    new v

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Home page</h1>
-    <div class="container">
+@extends('layouts.main')
+
+@section('main-content')
+    <div class="container text-center">
         <section class=table-section>
             <h2>All students</h2>
             @include('partials.table', ['students' => $allStudents])
         </section>
-
+        {{--
         <section class=table-section>
             <h2>Class 1 students</h2>
             @include('partials.table', ['students' => $class1])
@@ -32,6 +25,6 @@
             <h2>All students born in 1990</h2>
             @include('partials.table', ['students' => $born1990])
         </section>
+        --}}
     </div>
-</body>
-</html>
+@endsection
